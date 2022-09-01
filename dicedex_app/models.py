70105f6676a10a3a57b756bfe26399ddee8c1601
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-09-01 01:48:45 -0400

    Added authentication.

--- dicedex_app/models.py
+++ dicedex_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 # Create your models here.
 
@@ -26,6 +26,8 @@ class Game(models.Model):
     link = models.CharField(max_length=1000, default="None")
     timestamp = models.DateField(auto_now_add=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
 
     def __str__(self):
         return self.title
