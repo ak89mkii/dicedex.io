781b1b2a4efd5beb679d80f0f05db5f391803bfb
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-09-10 17:56:08 -0400

    make it better

--- dicedex/wsgi.py
+++ dicedex/wsgi.py
@@ -10,11 +10,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dicedex.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
+# application = WhiteNoise(application, root="/path/to/static/files")
+# application.add_files("/path/to/more/static/files", prefix="more-files/")
