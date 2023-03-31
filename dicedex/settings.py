56d494396e9a45d279f57c525dbcda0d6589d10d
ak89mkii <ak89mkii@gmail.com>
2023-03-31 13:08:49 -0400

    Togggled debug and db in settings for development.

--- dicedex/settings.py
+++ dicedex/settings.py
@@ -34,6 +34,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ["*"]
 
+# # Comment out for local development (set to true).
 # DEBUG = False
 DEBUG = True
 
@@ -92,9 +93,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+# # Comment out for local development.
+# import dj_database_url
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
